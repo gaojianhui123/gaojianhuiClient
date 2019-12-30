3b03243ead643553ac02683f19332996aee19e8a
gaojianhui <958226322@qq.com>
2019-12-30 11:22:38 +0800

    问题

--- src/app/model/Role.ts
+++ src/app/model/Role.ts
@@ -4,7 +4,6 @@ import {ProtectedResource} from './ProtectedResource';
 export class Role extends BaseEntity {
 
   protectedResources: Array<ProtectedResource> = new Array<ProtectedResource>();              //  权限
-  // protectedResources: any;
   roletype: string ;    // 角色类型
   errorMsg: string;
 
